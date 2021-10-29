f9938a8b96c213ba0a3cae17e67d8ffd2013a94a
Ale <callaandbandc@163.com>
2021-10-29 15:59:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <el-header style="height: 50px">
+      <el-row>
+        <el-button type="primary" v-on:click="elSideCollapse()">收起</el-button>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside :width="esWidth">
+        <Header :isCollapse=isCollapse></Header>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+    <!-- <img src="./assets/logo.png"> -->
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+
+export default {
+  components: { Header },
+  name: 'App',
+  data () {
+    return {
+      esWidth: '200px',
+      isCollapse: false
+    }
+  },
+  methods: {
+    elSideCollapse () {
+      this.isCollapse = !this.isCollapse
+
+      this.esWidth = this.isCollapse ? 'auto' : '200px'
+
+      console.log(this.isCollapse)
+    },
+    show: function () {
+      let screenWidth = document.body.clientWidth
+      let screenHeight = document.body.clientHeight
+      console.log(`${screenWidth},${screenHeight}`)
+    }
+  },
+  mounted () {
+    console.log('mounted is start')
+    this.show()
+  }
+}
+</script>
+
+<style>
+
+  body{
+    margin: 0;
+  }
+  .el-header, .el-footer {
+    background-color: #FFFFFF;
+    color: #333;
+    /*text-align: center;*/
+    line-height: 50px;
+  }
+  .el-aside {
+    background-color: #F4F5F9;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+  .el-main{
+    height: 100%;
+  }
+</style>
---
+++ src/components/NewHello.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="main" style="width: 600px;height: 400px;"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+
+export default {
+    name: 'NewHello',
+  data () {
+    return {
+      charts: '',
+      opinionData: ['3', '2', '4', '4', '5']
+    }
+  },
+  methods: {
+    drawLine (id) {
+      this.charts = echarts.init(document.getElementById(id))
+      this.charts.setOption({
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['近七日收益']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['1', '2', '3', '4', '5']
+        },
+        yAxis: {
+          type: 'value'
+        },
+
+        series: [{
+          name: '近七日收益',
+          type: 'line',
+          stack: '总量',
+          data: this.opinionData
+        }]
+      })
+    }
+  },
+  mounted () {
+    this.$nextTick(function () {
+      this.drawLine('main')
+    })
+  }
+}
+</script>
