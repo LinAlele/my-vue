7e5db2f8e0444ac8101aa64c615c40f9e4dbba4c
Ale <callaandbandc@163.com>
2021-11-03 10:58:53 +0800

    新增路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -4,18 +4,38 @@ import HelloWorld from '@/components/HelloWorld'
 import NewHello from '@/components/NewHello'
 
 Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
+const routes = [
+  {
+    path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/NewHello',
+    component: HelloWorld
+  },
+  {
+    path: '/NewHello',
       name: 'NewHello',
-      component: NewHello
-    }
-  ]
+    component: NewHello
+  }
+]
+
+const router = new Router({
+  routes
+})
+
+export default router
+
+// 路由发生之前
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach', to, from)
+  next()
+})
+
+// 路由解析之前被调用
+router.beforeResolve((to, from, next) => {
+  console.log('beforeResolve', to, from)
+  next()
+})
+
+// 路由进入之后触发事件
+router.afterEach((to, from) => {
+  console.log(to, from)
 })
